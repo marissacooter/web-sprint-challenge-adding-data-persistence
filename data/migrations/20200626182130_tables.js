7ecf96b4733e45055fe9317da9a378cdd167cfd3
Marissa <marissacooterl@gmail.com>
2020-06-26 18:26:48 -0500

    project table created and confirmed in sqlite browser

--- data/migrations/20200626182130_tables.js
+++ data/migrations/20200626182130_tables.js
@@ -1,8 +1,13 @@
 
 exports.up = async function(knex) {
-  
-};
+  await knex.schema.createTable("project", (table) => {
+      table.increments("id")
+      table.text("name").notNull()
+      table.text("description").notNull()
+      table.integer("completed", 0)
+  })
+}
 
 exports.down = async function(knex) {
   
-};
+}
