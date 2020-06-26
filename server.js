c558fd7d7139021054c1d9073e86c5ade5e9437c
Marissa <marissacooterl@gmail.com>
2020-06-26 18:15:49 -0500

    tested server, server running

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 const express = require("express")
+
+const server = express()
+
+server.use(express.json())
+
+// server.get('/', (req, res) => {
+//     res.status(200).json({ message: "we made it!"})
+// })
+
+module.exports = server 
