5fa43b95214fdec1b791c4c8e15dcaf313b08131
Marissa <marissacooterl@gmail.com>
2020-06-26 18:48:29 -0500

    join table created and confirmed in sqlite browser

--- data/migrations/20200626182130_tables.js
+++ data/migrations/20200626182130_tables.js
@@ -1,26 +1,35 @@
-
-exports.up = async function(knex) {
+exports.up = async function (knex) {
   await knex.schema.createTable("project", (table) => {
-      table.increments("id")
-      table.text("name").notNull()
-      table.text("description").notNull()
-      table.integer("completed", 0)
+    table.increments("id")
+    table.text("name").notNull()
+    table.text("description").notNull()
+    table.integer("completed", 0)
   })
 
   await knex.schema.createTable("resource", (table) => {
-      table.increments("id")
-      table.text("name").notNull()
-      table.text("description").notNull()
+    table.increments("id")
+    table.text("name").notNull()
+    table.text("description").notNull()
   })
 
   await knex.schema.createTable("task", (table) => {
-      table.increments("id")
-      table.text("description").notNull()
-      table.text("notes").notNull()
-      table.integer("completed", 0)
+    table.increments("id")
+    table.text("description").notNull()
+    table.text("notes").notNull()
+    table.integer("completed", 0)
+  })
+
+  await knex.schema.createTable("task_resource", (table) => {
+    table.increments("id")
+    table
+        .integer("task_id")
+        .references("id")
+        .inTable("task")
+    table
+        .integer("resource_id")
+        .references("id")
+        .inTable("resource")
   })
 }
 
-exports.down = async function(knex) {
-  
-}
+exports.down = async function (knex) {};
