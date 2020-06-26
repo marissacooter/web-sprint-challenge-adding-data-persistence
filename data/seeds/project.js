a6b11036face01bdf24def247d771c744cf82d14
Marissa <marissacooterl@gmail.com>
2020-06-26 18:57:17 -0500

    project table seeded

---
+++ data/seeds/project.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('project').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('project').insert([
+        { name: 'Marissa', description: 'Description One', completed: false},
+        { name: 'Martha', description: 'Description Two', completed: false},
+        { name: 'Maddie', description: 'Description Three', completed: false}
+      ]);
+    });
+};
