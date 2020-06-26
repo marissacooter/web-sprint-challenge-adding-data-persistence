cd7966c33fdeae380945980fc3706aa1246e2bf4
Marissa <marissacooterl@gmail.com>
2020-06-26 18:20:11 -0500

    knexfile and data folder with config file set up

---
+++ knexfile.js
@@ -0,0 +1,23 @@
+module.exports = {
+	development: {
+		client: "sqlite3",
+		useNullAsDefault: true, 
+		connection: {
+			filename: "./data/project.db3",
+		},
+		migrations: {
+			directory: "./data/migrations",
+		},
+		seeds: {
+			directory: "./data/seeds",
+		},
+
+	
+		pool: {
+			afterCreate: (conn, done) => {
+				
+				conn.run("PRAGMA foreign_keys = ON", done) // turn on FK enforcement
+			},
+		},
+	},
+}
