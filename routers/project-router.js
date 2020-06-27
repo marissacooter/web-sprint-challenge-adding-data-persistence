0e4ef0e76fc2ad5203298a8563ec6e1172f75214
Marissa <marissacooterl@gmail.com>
2020-06-26 20:05:51 -0500

    project router done

--- routers/project-router.js
+++ routers/project-router.js
@@ -3,5 +3,43 @@ const project = require('../models/project-model')
 
 const router = express.Router()
 
+// GET
+router.get('/', (req, res) => {
+    project.find()
+    .then(project => {
+        res.json(project)
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'Failed to get project.'})
+    })
+})
+
+// GET :id
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+    project.findById(id)
+    .then(project => {
+        if (project) {
+            res.json(project)
+        } else {
+            res.status(404).json({ message: 'Could not find project with the given ID.'})
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'Failed to get project.'})
+    })
+})
+
+// POST
+router.post('/', (req, res) => {
+    const projectData = req.body
+    project.addProject(projectData)
+    .then(project => {
+        res.status(201).json(project)
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'Failed to create new project.'})
+    })
+})
 
 module.exports = router
