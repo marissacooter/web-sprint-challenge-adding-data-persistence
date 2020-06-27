14e98bb09f2e9ca94cfa34934d59aded886a0443
Marissa <marissacooterl@gmail.com>
2020-06-26 19:50:41 -0500

    project router stubbed out

---
+++ routers/project-router.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const project = require('../models/project-model')
+
+const router = express.Router()
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express")
+const projectRouter = require('./routers/project-router')
 
 const server = express()
 
 server.use(express.json())
+server.use('/api/project', projectRouter)
 
 server.get('/', (req, res) => {
     res.status(200).json({ message: "My Server is running!"})
